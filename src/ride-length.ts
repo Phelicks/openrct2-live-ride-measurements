7ff77311ee82183bb71e06c1c0bebe743b255a54
Felix <FeJa-92@gmx.de>
2021-03-03 20:58:53 +0100

    Add more measurements

--- src/ride-length.ts
+++ src/ride-length.ts
@@ -1,6 +1,10 @@
 import { RideMeasurements } from "./ride-measurements"
 import { Measurements, RideMeasurementsWindow } from "./ride-measurements-window"
 
+// Doesn't support rides with multiple stations
+// G calculation is off
+// Ghost trains need to be enabled
+
 registerPlugin({
     name: "Ride Info",
     version: "1.0",
@@ -35,10 +39,14 @@ function openRideMeasurementsWindow() {
 
         rideMeasurements.update()
 
-        rideMeasurementsWindow.setValue(Measurements.rideLength, (rideMeasurements.maxLength >> 16) + "m")
-        rideMeasurementsWindow.setValue(Measurements.positiveGs, (rideMeasurements.maxVerticalPosG / 100).toFixed(2) + "g")
-        rideMeasurementsWindow.setValue(Measurements.negativeGs, (rideMeasurements.maxVerticalNegG / 100).toFixed(2) + "g")
-        rideMeasurementsWindow.setValue(Measurements.lateralGs, (rideMeasurements.maxLateralG / 100).toFixed(2) + "g")
+        rideMeasurementsWindow.setValue(Measurements.maxSpeed, mphToKmph((rideMeasurements.maxSpeed.value * 9) >> 18) + " km/h")
+        rideMeasurementsWindow.setValue(Measurements.rideLength, (rideMeasurements.maxLength.value >> 16) + " m")
+        rideMeasurementsWindow.setValue(Measurements.positiveGs, (rideMeasurements.maxVerticalPosG.value / 100).toFixed(2) + " g")
+        rideMeasurementsWindow.setValue(Measurements.negativeGs, (rideMeasurements.maxVerticalNegG.value / 100).toFixed(2) + " g")
+        rideMeasurementsWindow.setValue(Measurements.lateralGs, (rideMeasurements.maxLateralG.value / 100).toFixed(2) + " g")
+        rideMeasurementsWindow.setValue(Measurements.airTime, (rideMeasurements.totalAirTime.value * 3 / 100).toFixed(2) + " secs")
+        rideMeasurementsWindow.setValue(Measurements.averageSpeed, mphToKmph(((rideMeasurements.averageSpeed.value / rideMeasurements.time.value) * 9) >> 18) + " km/h")
+        rideMeasurementsWindow.setValue(Measurements.rideTime, (rideMeasurements.time.value) + " secs")
     })
 
     rideMeasurementsWindow.open(tickHook.dispose, (index) => {
@@ -46,3 +54,5 @@ function openRideMeasurementsWindow() {
     });
     rideMeasurementsWindow.dropdownContent = rideNames
 }
+
+const mphToKmph = (mph: number) => (mph * 1648) >> 10;
